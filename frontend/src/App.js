00907899f150f5c1e8cc70e58523ff3718e85f19
Sumit K <synergy2411@outlook.com>
2022-06-30 15:43:40 +0530

    react app deployed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { Redirect, Route } from 'react-router-dom';
 import HelloGraphql from "./Components/Demo/HelloGraphql";
 import Login from "./Components/Demo/Login";
-import Expenses from './Components/Expenses/Expenses';
+// import Expenses from './Components/Expenses/Expenses';
 import Header from './Components/Header/Header';
 import Profile from './Components/Profile/Profile';
 import { AuthContext } from './context/auth-context';
+
+const Expenses = React.lazy(() => import("./Components/Expenses/Expenses"))
 
 function App() {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   useEffect(() => {
-    if(localStorage.getItem("authToken")){
+    if (localStorage.getItem("authToken")) {
       setIsLoggedIn(true)
     }
   }, [])
@@ -23,25 +25,27 @@ function App() {
       isLoggedIn,
       setIsLoggedIn
     }}>
-    <div className="container">
-      <Header />
-      <br />
-      <Route path="/" exact>
-        <Redirect to="/login" />
-      </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/expenses">
-        <Expenses />
-      </Route>
-      <Route path="/hello-graphql">
-        <HelloGraphql />
-      </Route>
-      <Route path="/profile">
-        <Profile />
-      </Route>
-    </div>
+      <div className="container">
+        <Header />
+        <br />
+        <Suspense fallback={<p>Loading...</p>}>
+          <Route path="/" exact>
+            <Redirect to="/login" />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/expenses">
+            <Expenses />
+          </Route>
+          <Route path="/hello-graphql">
+            <HelloGraphql />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Suspense>
+      </div>
     </AuthContext.Provider>
   );
 }
